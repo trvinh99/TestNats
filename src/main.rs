f49568ecbf088f6ac7a4ca243a458fa13b6bf521
trvinh99 <48145607+trvinh99@users.noreply.github.com>
2021-11-08 17:08:22 +0700

    leak mem

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ mod throttle;
 
 use bastion::Bastion;
 use bastion::{run, spawn};
+use chrono::Utc;
 use log::LevelFilter;
 use log::{info, trace, warn};
 use smol::Timer;
@@ -12,6 +13,13 @@ use std::fs::File;
 use std::io::Read;
 use std::time::Duration;
 use std::time::SystemTime;
+
+use std::{
+    convert::{TryFrom, TryInto},
+    io::Write,
+    str::FromStr,
+    sync::Arc,
+};
 
 use crate::test_actor::TestActor;
 
@@ -21,43 +29,67 @@ fn main() {
     Bastion::init();
     Bastion::start();
 
-    let mut file = File::open("src/logo.png").unwrap();
-    let mut contents = vec![];
-    file.read_to_end(&mut contents).unwrap();
+    let record_db_config = sled::Config::default()
+        .path(format!("src/record/{}", 1))
+        .cache_capacity(10 * 1024 * 1024)
+        // .flush_every_ms(Some(200))
+        .mode(sled::Mode::HighThroughput);
+    let record_db = record_db_config.open().unwrap();
 
-    for i in 1..=30 {
-        let contents = contents.clone();
-        let record_db_config = sled::Config::default()
-            .path(format!("src/record/{}", i))
-            .cache_capacity(10 * 1024 * 1024)
-            // .flush_every_ms(Some(200))
-            .mode(sled::Mode::HighThroughput);
-        let record_db = record_db_config.open().unwrap();
-        spawn!(async move {
-            let mut i = 0;
-            while i < 20 {
-                let mut batch = sled::Batch::default();
-                let mut j = 0;
-                while j < 10 {
-                    let now = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
-                        Ok(n) => n.as_nanos(),
-                        Err(_) => panic!("SystemTime before UNIX EPOCH!"),
-                    };
+    let frame = record_db.first().unwrap().unwrap();
+    let frame_key_str = String::from_utf8(frame.0.to_vec()).unwrap();
+    let start = i64::from_str(&frame_key_str).unwrap();
+    let end = (start / 1_000_000_000i64) + 20;
 
-                    let _ = batch.insert(now.to_string().as_bytes(), contents.to_vec());
-                    println!("J: {}", j);
+    let start_str = start.to_string();
+    let start_bin = start_str.as_bytes();
+    let end_str = end.to_string();
+    let end_bin = end_str.as_bytes();
 
-                    j += 1;
+    let bef = Utc::now().timestamp();
 
-                    Timer::after(Duration::from_millis(200)).await;
-                }
-                println!("I: {}", j);
-                i += 1;
+    let data = record_db.range(start_bin..end_bin);
 
-                record_db.apply_batch(batch).unwrap();
-            }
-        });
-    }
+    let aft = Utc::now().timestamp();
+    println!("Query time: {}", aft - bef);
+
+    // let mut file = File::open("src/logo.png").unwrap();
+    // let mut contents = vec![];
+    // file.read_to_end(&mut contents).unwrap();
+
+    // for i in 1..=30 {
+    //     let contents = contents.clone();
+    //     let record_db_config = sled::Config::default()
+    //         .path(format!("src/record/{}", i))
+    //         .cache_capacity(10 * 1024 * 1024)
+    //         // .flush_every_ms(Some(200))
+    //         .mode(sled::Mode::HighThroughput);
+    //     let record_db = record_db_config.open().unwrap();
+    //     spawn!(async move {
+    //         let mut i = 0;
+    //         while i < 20 {
+    //             let mut batch = sled::Batch::default();
+    //             let mut j = 0;
+    //             while j < 10 {
+    //                 let now = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
+    //                     Ok(n) => n.as_nanos(),
+    //                     Err(_) => panic!("SystemTime before UNIX EPOCH!"),
+    //                 };
+
+    //                 let _ = batch.insert(now.to_string().as_bytes(), contents.to_vec());
+    //                 println!("J: {}", j);
+
+    //                 j += 1;
+
+    //                 Timer::after(Duration::from_millis(200)).await;
+    //             }
+    //             println!("I: {}", j);
+    //             i += 1;
+
+    //             record_db.apply_batch(batch).unwrap();
+    //         }
+    //     });
+    // }
 
     // //let url = "10.50.13.185:4222".to_string();
     // //let url = "10.50.13.181:4222".to_string();
