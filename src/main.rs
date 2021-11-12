d318b0d767d53c1a2fc4e7c5027552d4fe2c708f
trvinh99 <48145607+trvinh99@users.noreply.github.com>
2021-11-12 15:41:46 +0700

    ledb

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use bastion::spawn;
 use bastion::Bastion;
 use chrono::Utc;
+use ledb::Collection;
 use serde::Deserialize;
 use serde::Serialize;
 use sled::Db;
@@ -30,7 +31,7 @@ struct MyDoc {
     id: Option<Primary>,
     #[document(index)]
     timestamp: i64,
-    frame: String,
+    frame: Vec<u8>,
 }
 fn main() {
     // let config = config::Config::from_args(std::env::args()).unwrap();
@@ -39,7 +40,7 @@ fn main() {
     Bastion::start();
 
     insert();
-    //spawn!(query(1636432243220342000, 1636493293220342000));
+    // pawn!(query_db(1636637808736768110, 1636957818736768110));
 
     // let path = format!("src/record/{}", 1);
     // let storage = Storage::new(&path, Options::default()).unwrap();
@@ -58,7 +59,7 @@ fn main() {
     //     Err(_) => panic!("SystemTime before UNIX EPOCH!"),
     // };
 
-    // let filter = query!(@filter timestamp in 1636637808736768110..1636637818736768110);
+    // let filter = query!(@filter timestamp in 1636637808736768110..1636657818736768110);
     // let elements: Vec<MyDoc> = collection
     //     .find(filter, Order::Primary(OrderKind::Asc))
     //     .unwrap()
@@ -69,7 +70,14 @@ fn main() {
     // println!("last id: {}", last_id);
 
     // let last_frame: MyDoc = collection.get(last_id).unwrap().unwrap();
-    // println!("last timestamp: {}", last_frame.timestamp);
+    // let url = last_frame.frame;
+
+    // let mut f = File::open(&url).expect("no file found");
+    // let metadata = fs::metadata(&url).expect("unable to read metadata");
+    // let mut buffer = vec![0; metadata.len() as usize];
+    // f.read(&mut buffer).expect("buffer overflow");
+
+    // println!("last timestamp: {:?}", buffer);
 
     // let aft = match SystemTime::now().duration_since(SystemTime::UNIX_EPOCH) {
     //     Ok(n) => n.as_nanos(),
@@ -99,7 +107,7 @@ fn insert() {
         // let record_db: DB = DB::open(&db_opts, path).unwrap();
         // Open storage
         let storage = Storage::new(&path, Options::default()).unwrap();
-        unsafe { storage.set_mapsize(1024 * 1024 * 1024 * 30) };
+        unsafe { storage.set_mapsize(1024 * 1024 * 1024 * 25) };
 
         // Get collection
         let collection = storage.collection("record").unwrap();
@@ -114,22 +122,20 @@ fn insert() {
                     Ok(n) => n.as_nanos(),
                     Err(_) => panic!("SystemTime before UNIX EPOCH!"),
                 };
-                //if !record_db.contains_key(now.to_string().as_bytes()).unwrap() {
-                // let _ = record_db.put(now.to_string().as_bytes(), contents.to_vec());
 
-                let folder_url = format!("src/record_frame/{}/{}", "2021/11/12", i);
-                fs::create_dir_all(&folder_url).unwrap();
+                // let folder_url = format!("src/record_frame/{}/{}", "2021/11/12", i);
+                // fs::create_dir_all(&folder_url).unwrap();
 
-                let file_url = format!("src/record_frame/{}/{}/{}", "2021/11/12", i, now);
+                // let file_url = format!("src/record_frame/{}/{}/{}", "2021/11/12", i, now);
 
-                let mut file = File::create(file_url.clone()).unwrap();
-                file.write_all(&contents).unwrap();
+                // let mut file = File::create(file_url.clone()).unwrap();
+                // file.write_all(&contents).unwrap();
 
                 let first_id = collection
                     .insert(&MyDoc {
                         id: None,
                         timestamp: now as i64,
-                        frame: file_url,
+                        frame: contents.to_vec(),
                     })
                     .unwrap();
 
@@ -145,19 +151,25 @@ fn insert() {
 
 async fn query_db(start_time: i64, end_time: i64) {
     println!("QUERYYY");
-    let record_db_config = sled::Config::default()
-        .path(format!("src/record/{}", 1))
-        .cache_capacity(10 * 1024 * 1024)
-        // .flush_every_ms(Some(200))
-        .mode(sled::Mode::HighThroughput);
-    let record_db = record_db_config.open().unwrap();
+    let path = format!("src/record/{}", 1);
+    let storage = Storage::new(&path, Options::default()).unwrap();
+
+    // Get collection
+    let collection = storage.collection("record").unwrap();
+
+    // Ensure indexes
+    query!(index for collection
+        timestamp int unique,
+    )
+    .unwrap();
+
     let mut sleep = 0;
 
     let mut limit_step = 0;
 
-    let start_record = get_start_record_time(record_db.clone());
+    let start_record = get_start_record_time(collection.clone());
     println!("Start record: {}", start_record);
-    let end_record = get_end_record_time(record_db.clone());
+    let end_record = get_end_record_time(collection.clone());
     println!("End record: {}", end_record);
 
     if start_time <= end_record && end_time >= start_record && start_time <= end_time {
@@ -176,19 +188,14 @@ async fn query_db(start_time: i64, end_time: i64) {
         println!("Start time: {} and End time: {}", start, end);
 
         while start <= end {
-            let record_db = record_db.clone();
+            let collection = collection.clone();
             limit_step = if end - start >= limit_step {
                 LIMIT_STEP
             } else {
                 end - start + ONE_SEC
             };
 
-            let frames = range_query(
-                record_db,
-                start.to_string().as_bytes(),
-                (start + limit_step).to_string().as_bytes(),
-                1,
-            );
+            let frames = range_query(collection, start, start + limit_step, 1);
             println!("Vec length: {}", frames.len());
 
             let mut cur_index = 0;
@@ -211,41 +218,35 @@ async fn query_db(start_time: i64, end_time: i64) {
     }
 }
 
-pub fn range_query(db: Db, start: &[u8], end: &[u8], speed: usize) -> Vec<(i64, Vec<u8>)> {
-    let query = db.range(start..end);
+pub fn range_query(
+    collection: Collection,
+    start: i64,
+    end: i64,
+    speed: usize,
+) -> Vec<(i64, Vec<u8>)> {
+    let filter = query!(@filter timestamp in start..end);
+    let elements: Vec<MyDoc> = collection
+        .find(filter, Order::Primary(OrderKind::Asc))
+        .unwrap()
+        .collect::<Result<Vec<_>, _>>()
+        .unwrap();
     let mut value_array: Vec<(i64, Vec<u8>)> = Vec::<(i64, Vec<u8>)>::new();
-    for q in query.step_by(speed) {
-        if let Ok(tuple_res) = q {
-            let timestamp_str = String::from_utf8(tuple_res.0.to_vec()).unwrap();
-            let timestamp = i64::from_str(&timestamp_str).unwrap();
-            let data = tuple_res.1.to_vec();
-
-            value_array.push((timestamp, data));
-        } else {
-            break;
-        }
+    for q in elements.into_iter().step_by(speed) {
+        value_array.push((q.timestamp, q.frame));
     }
     value_array
 }
 
-pub fn get_start_record_time(db: Db) -> i64 {
-    let result = match db.first().unwrap() {
-        Some(frame) => {
-            let frame_key_str = String::from_utf8(frame.0.to_vec()).unwrap();
-            i64::from_str(&frame_key_str).unwrap()
-        }
-        None => 0,
-    };
-    result
+pub fn get_start_record_time(collection: Collection) -> i64 {
+    let first_id = 1;
+
+    let first_frame: MyDoc = collection.get(first_id).unwrap().unwrap();
+    first_frame.timestamp
 }
 
-pub fn get_end_record_time(db: Db) -> i64 {
-    let result = match db.last().unwrap() {
-        Some(frame) => {
-            let frame_key_str = String::from_utf8(frame.0.to_vec()).unwrap();
-            i64::from_str(&frame_key_str).unwrap()
-        }
-        None => 0,
-    };
-    result
+pub fn get_end_record_time(collection: Collection) -> i64 {
+    let last_id = collection.last_id().unwrap();
+
+    let last_frame: MyDoc = collection.get(last_id).unwrap().unwrap();
+    last_frame.timestamp
 }
