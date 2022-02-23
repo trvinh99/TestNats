1bb167c7df78c190dcc9467f2de20d972dc4556c
LexRay <lexray@localhost.localdomain>
2022-02-23 01:25:34 -0600

    Update data test

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cat /dev/null > logs/log.txt
-totalCams=30
+totalCams=20
 #totalCams=1
-totalMsg=3000
+totalMsg=2000
 url=tls://stage-nats.lexray.com:4222
 fps=3
 ./target/release/TestNats --max-msg=$totalMsg --publisher-actors=1 --max-cams=$totalCams  --nats-url=$url --fps=$fps
