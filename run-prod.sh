005529253eb676899e622097c26451f19d2814d2
ngoclinh93.qt <lexray@hubbox.local>
2022-04-12 05:34:23 -0400

    script to run test

--- run-prod.sh
+++ run-prod.sh
@@ -3,7 +3,7 @@ cat /dev/null > logs/log.txt
 totalCams=20
 #totalCams=1
 totalMsg=2000
-#totalMsg=1
+totalMsg=100
 url=tls://nats.lexray.com:4222
 fps=3
 ./target/release/TestNats --max-msg=$totalMsg --publisher-actors=1 --max-cams=$totalCams  --nats-url=$url --fps=$fps
