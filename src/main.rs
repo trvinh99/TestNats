74206f861353f0040f3ff5f88f20531d9b0f3247
trvinh99 <48145607+trvinh99@users.noreply.github.com>
2022-05-26 17:44:36 +0700

    p

--- src/main.rs
+++ src/main.rs
@@ -230,7 +230,7 @@ fn start_pipeline(root_path: String) -> Result<(), anyhow::Error> {
     gst::init()?;
 
     let pipeline = gst::parse_launch(
-        &format!("rtspsrc location=rtsp://10.50.13.252/1/h264major ! rtph264depay !  vaapih264dec ! videoconvert !  x264enc ! mpegtsmux ! multifilesink max-files=5 max-file-duration=5000000000 post-messages=true next-file=2 location={}/hls/ch%05d.ts", root_path)
+        &format!("rtspsrc location=rtsp://10.50.13.252/1/h264major ! rtph264depay !  vaapih264dec ! videoconvert !  x264enc ! mpegtsmux ! multifilesink max-files=5 max-file-duration=5000000000 post-messages=true next-file=5 location={}/hls/ch%05d.ts", root_path)
 
         // &format!("rtspsrc location=rtsp://10.50.13.252/1/h264major ! rtph264depay ! vaapih264dec ! videoconvert !  x264enc ! mpegtsmux ! hlssink playlist-location={}/m3u8/hlstest.m3u8 location={}/hls/ch%05d.ts message-forward=true target-duration=6", root_path, root_path)
     )?;
